0c6ba8b24df4ac0bbc1f3a96edd1a37310593a8f
hamed diop <diopmuha@gmail>
2023-09-17 22:13:16 -0500

    added navbar function

--- src/App.js
+++ src/App.js
@@ -9,32 +9,13 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              testtttinggggggg
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Box textAlign="flex-end" fontSize="xl" backgroundColor='blue' justifyContent="flex-start">
+    </Box>
     </ChakraProvider>
   );
 }
