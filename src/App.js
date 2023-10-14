a39131bb696e1df4bed725f1c07ac6e3c448bd52
hamed diop <diopmuha@gmail>
2023-10-13 21:24:21 -0500

    added shadows

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import {
   Grid,
   theme,
   Flex,
+  Card,
+  CardBody,
 } from '@chakra-ui/react';
 import Lefthome from './LeftHome';
 import Righthome from './RightHome';
@@ -18,21 +20,36 @@ import Righthome from './RightHome';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}> 
-      <Box textAlign="flex-end" fontSize="xl" backgroundColor='#cbcbd4' justifyContent="flex-start" maxWidth={200} maxH={500} paddingLeft="10" borderRadius="20px" maxHeight="10" marginTop={3} marginLeft={7} textColor='black'>  
+    <ChakraProvider theme={theme} > 
+    <Box>
+      <Box boxShadow='dark-lg'textAlign="flex-end" fontSize="xl" backgroundColor='#FFFFFF' justifyContent="flex-start" maxWidth={200} maxH={500} paddingLeft="10" borderRadius="20px" maxHeight="10" marginTop={3} marginLeft={7} textColor='black'>  
         <Text> TrainWithUs.</Text>
         
     </Box>
     <Flex>
       <Box>
           <Lefthome/>
+          
       </Box>
-      <Box maxWidth={800}>
-          <Righthome/>
-          <Righthome/>
-          <Righthome/>
+      <Box  maxWidth={800}>
+        <Card  boxShadow='dark-lg' marginTop={70} marginLeft={140} height={100} width={600} backgroundColor="#FFFFFF">
+            <CardBody>
+                 <Text alignContent='center' textColor='black' fontSize="xl" fontWeight="bolder" marginTop="3.5">Use our personalized fitness routines to hit your goals.</Text>
+            </CardBody>
+        </Card>
+        <Card boxShadow='dark-lg' marginTop={70} marginLeft={140} height={100} width={600} backgroundColor="#FFFFFF">
+            <CardBody>
+              <Text alignContent='center' textColor='black' fontSize="xl" fontWeight="bolder" marginTop="3.5">Get your summer body using our BMR calculator.</Text>
+            </CardBody>
+        </Card>
+        <Card boxShadow='dark-lg' marginTop={70} marginLeft={140} height={100} width={600} backgroundColor="#FFFFFF">
+          <CardBody>
+            <Text alignContent='center' textColor='black' fontSize="xl" fontWeight="bolder" marginTop="3.5">Utilize our built in demonstrations to visualize workouts.</Text>
+          </CardBody>
+        </Card>
         </Box>
         </Flex>
+        </Box>
     </ChakraProvider>
   );
 }
--- src/LeftHome.js
+++ src/LeftHome.js
@@ -8,14 +8,15 @@ import {
   Code,
   Grid,
   theme,
+  
 } from '@chakra-ui/react';
    
 
 export default function Lefthome ()  {
  return (
-    <Box backgroundColor='#cbcbd4' borderRadius="10px" width={400} height={400} marginTop={100} marginLeft={7} textAlign="center" textColor="black" justifyItems="center" fontWeight='bolder' paddingTop="50px"> 
-        <Text fontSize="l"> Meet your fitness goals with <span> TrainWithUs.</span></Text>
-        <Text paddingTop={12} fontSize="l" paddingLeft={10} paddingRight={10}>Improve your nutrition,
+    <Box boxShadow='dark-lg' backgroundColor='#FFFFFF' borderRadius="10px" width={400} height={440} marginTop={100} marginLeft={7} textAlign="center" textColor="black" justifyItems="center" fontWeight='bolder' paddingTop="50px" paddingLeft="10px" paddingBottom="20px"> 
+        <Text fontSize="xl"> Meet your fitness goals with <span> TrainWithUs.</span></Text>
+        <Text paddingTop={12} fontSize="xl" paddingLeft={10} paddingRight={10}>Improve your nutrition,
          build strength through weightlifting or cardio.
          <Text paddingTop={12}>  TrainWithUs offers 
           nutrition plans, a diverse range of workout routines, and expert guidance to help you achieve your fitness goals. 
