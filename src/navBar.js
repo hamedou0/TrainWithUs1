4540c8579973cd2c48564eaf94bca00485cfc8f8
hamed diop <diopmuha@gmail>
2023-10-20 23:04:29 -0500

    Commit made from VS Code Azure Static Web Apps

--- src/navBar.js
+++ src/navBar.js
@@ -24,14 +24,16 @@ import {
             TrainWithUs. 
           </Text>
     </Box>
-    <a href='https://www.notion.so/trainwithusnutritionhandbook/db732843e92245349fa3af50414df3d3?v=85ea0354207b438d8beabb23f45ac5c6'>
+    
     <Button hrefboxShadow='base' textAlign="flex-end" fontSize="xl" backgroundColor='#FFFFFF' justifyContent="flex-start" maxWidth={200} maxH={500} paddingLeft={6} borderRadius="20px" maxHeight="10" marginTop={3} marginLeft={20} textColor='black' paddingRight={6}>  
-           <Text> Nutrition.</Text>
+           <Text> Lifestyle.</Text>
+    </Button>
+    
+    <a href='https://www.notion.so/trainwithusnutritionhandbook/db732843e92245349fa3af50414df3d3?v=85ea0354207b438d8beabb23f45ac5c6'>
+    <Button boxShadow='base' textAlign="flex-end" fontSize="xl" backgroundColor='#FFFFFF' justifyContent="flex-start" maxWidth={200} maxH={500} paddingLeft={6} borderRadius="20px" maxHeight="10" marginTop={3} marginLeft={35} textColor='black'paddingRight={6}>  
+          <Text> Nutrition.</Text>
     </Button>
     </a>
-    <Button boxShadow='base' textAlign="flex-end" fontSize="xl" backgroundColor='#FFFFFF' justifyContent="flex-start" maxWidth={200} maxH={500} paddingLeft={6} borderRadius="20px" maxHeight="10" marginTop={3} marginLeft={35} textColor='black'paddingRight={6}>  
-          <Text> Lifestyle.</Text>
-    </Button>
     <Button boxShadow='base' textAlign="flex-end" fontSize="xl" backgroundColor='#FFFFFF' justifyContent="flex-start" maxWidth={200} maxH={500} paddingLeft={6} borderRadius="20px" maxHeight="10" marginTop={3} marginLeft={35} textColor='black'paddingRight={6}>  
           <Text> Exercise.</Text>
     </Button>
